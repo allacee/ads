c363e9d3c817d72ad84afe582cdaa14b07736e29
allacee <ddimaglushkov@ya.ru>
2019-03-14 12:45:49 +0300

    1401 updated

--- MODULE_1/P3210_DGlushkov_1401.cpp
+++ MODULE_1/P3210_DGlushkov_1401.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <math.h>
 
-//number of next figure
-static uint16_t num = 0;
+//number for next figure
+static int num = 0;
 
-typedef struct{
-    uint32_t raw;
-    uint32_t col;
+typedef struct
+{
+    int raw;
+    int col;
 } coord_t;
 
-void create_figure(uint32_t ** matrix, coord_t point)
+void create_even_figure(int ** matrix, coord_t point)
 {
 
     coord_t is_even = {point.raw % 2, point.col % 2};
@@ -19,12 +19,6 @@ void create_figure(uint32_t ** matrix, coord_t point)
         matrix[point.raw - 1][point.col - 1]
                 = matrix[point.raw - 1][point.col]
                 = matrix[point.raw][point.col - 1]
-                = ++num;
-
-    else if(!is_even.raw && !is_even.col)
-        matrix[point.raw + 1][point.col + 1]
-                = matrix[point.raw + 1][point.col]
-                = matrix[point.raw][point.col + 1]
                 = ++num;
 
     else if(!is_even.raw && is_even.col)
@@ -39,66 +33,90 @@ void create_figure(uint32_t ** matrix, coord_t point)
                 = matrix[point.raw - 1][point.col + 1]
                 = ++num;
 
-
+    else if(!is_even.raw && !is_even.col)
+        matrix[point.raw + 1][point.col + 1]
+                = matrix[point.raw + 1][point.col]
+                = matrix[point.raw][point.col + 1]
+                = ++num;
 
 }
 
-void solve(uint32_t ** matrix, uint32_t side_size, coord_t start, coord_t point)
+void create_figure(int ** matrix, coord_t point, int new_side_size, const int type)
+{
+    switch(type)
+    {
+        case (1):
+            matrix[point.raw + new_side_size - 1][point.col + new_side_size - 1]
+                    = matrix[point.raw + new_side_size][point.col + new_side_size - 1] 
+                    = matrix[point.raw + new_side_size - 1][point.col + new_side_size]
+                    = ++num;
+            break;
+
+        case (2):
+            matrix[point.raw + new_side_size][point.col + new_side_size] 
+                    = matrix[point.raw + new_side_size][point.col + new_side_size - 1] 
+                    = matrix[point.raw + new_side_size - 1][point.col + new_side_size] 
+                    = ++num;
+            break;
+
+        case (3):
+            matrix[point.raw + new_side_size][point.col + new_side_size] 
+                    = matrix[point.raw + new_side_size][point.col + new_side_size - 1] 
+                    = matrix[point.raw + new_side_size - 1][point.col + new_side_size - 1] 
+                    = ++num;
+            break;
+
+        case (4):
+            matrix[point.raw + new_side_size][point.col + new_side_size]                    
+                    = matrix[point.raw + new_side_size - 1][point.col + new_side_size]
+                    = matrix[point.raw + new_side_size - 1][point.col + new_side_size - 1] 
+                    = ++num;
+            break;
+    }
+}
+
+void solve(int ** matrix, int side_size, coord_t start, coord_t point)
 {
 
     if(side_size == 2)
     {
-        create_figure(matrix, {start.raw + point.raw % 2, start.col + point.col % 2});
+        create_even_figure(matrix, {start.raw + point.raw % 2, start.col + point.col % 2});
         return;
     }
-    uint32_t new_side_size = side_size / 2;
 
-    if (point.raw>= new_side_size && point.col >= new_side_size) {
+    int new_side_size = side_size / 2;
+
+    if(point.raw >= new_side_size && point.col >= new_side_size)
+    {
         solve(matrix, new_side_size, {start.raw + new_side_size, start.col + new_side_size}, {point.raw - new_side_size, point.col - new_side_size});
-
-        matrix[start.raw + new_side_size - 1][start.col + new_side_size - 1] =
-        matrix[start.raw + new_side_size][start.col + new_side_size - 1] =
-        matrix[start.raw + new_side_size - 1][start.col + new_side_size] = ++num;
-
+        create_figure(matrix, start, new_side_size, 1);
         solve(matrix, new_side_size, {start.raw, start.col + new_side_size}, {new_side_size - 1, 0});
-        solve(matrix, new_side_size, {start.raw, start.col}, {new_side_size - 1, new_side_size - 1});
+        solve(matrix, new_side_size, start, {new_side_size - 1, new_side_size - 1});
         solve(matrix, new_side_size, {start.raw + new_side_size, start.col}, {0, new_side_size - 1});
-
     }
-    else if (point.raw < new_side_size && point.col < new_side_size) {
-        solve(matrix, new_side_size, {start.raw, start.col}, { point.raw, point.col});
-
-        matrix[start.raw + new_side_size][start.col + new_side_size] =
-        matrix[start.raw + new_side_size][start.col + new_side_size - 1] =
-        matrix[start.raw + new_side_size - 1][start.col + new_side_size] = ++num;
-
-        solve(matrix, new_side_size, {start.raw + new_side_size, start.col + new_side_size}, { 0, 0});
-        solve(matrix, new_side_size, {start.raw, start.col + new_side_size}, { new_side_size - 1, 0});
-        solve(matrix, new_side_size, {start.raw + new_side_size, start.col}, { 0, new_side_size - 1});
-
+    else if(point.raw < new_side_size && point.col < new_side_size)
+    {
+        solve(matrix, new_side_size, start, point);
+        create_figure(matrix, start, new_side_size, 2);
+        solve(matrix, new_side_size, {start.raw + new_side_size, start.col + new_side_size}, {0, 0});
+        solve(matrix, new_side_size, {start.raw, start.col + new_side_size}, {new_side_size - 1, 0});
+        solve(matrix, new_side_size, {start.raw + new_side_size, start.col}, {0, new_side_size - 1});
     }
-    else if (point.raw < new_side_size && point.col >= new_side_size) {
-        solve(matrix, new_side_size, {start.raw, start.col + new_side_size}, { point.raw, point.col - new_side_size});
-
-        matrix[start.raw + new_side_size][start.col + new_side_size] =
-        matrix[start.raw + new_side_size][start.col + new_side_size - 1] =
-        matrix[start.raw + new_side_size - 1][start.col + new_side_size - 1] = ++num;
-
-        solve(matrix, new_side_size, {start.raw, start.col}, { new_side_size - 1, new_side_size - 1});
-        solve(matrix, new_side_size, {start.raw + new_side_size, start.col + new_side_size}, { 0, 0});
-        solve(matrix, new_side_size, {start.raw + new_side_size, start.col}, { 0, new_side_size - 1});
-
+    else if(point.raw < new_side_size && point.col >= new_side_size)
+    {
+        solve(matrix, new_side_size, {start.raw, start.col + new_side_size}, {point.raw, point.col - new_side_size});
+        create_figure(matrix, start, new_side_size, 3);
+        solve(matrix, new_side_size, start, {new_side_size - 1, new_side_size - 1});
+        solve(matrix, new_side_size, {start.raw + new_side_size, start.col + new_side_size}, {0, 0});
+        solve(matrix, new_side_size, {start.raw + new_side_size, start.col}, {0, new_side_size - 1});
     }
-    else {
-        solve(matrix, new_side_size, {start.raw + new_side_size, start.col}, { point.raw - new_side_size, point.col});
-
-        matrix[start.raw + new_side_size][start.col + new_side_size] =
-        matrix[start.raw + new_side_size - 1][start.col + new_side_size] =
-        matrix[start.raw + new_side_size - 1][start.col + new_side_size - 1] = ++num;
-
-        solve(matrix, new_side_size, {start.raw, start.col}, {new_side_size - 1, new_side_size - 1});
-        solve(matrix, new_side_size, {start.raw + new_side_size, start.col + new_side_size}, { 0, 0});
-        solve(matrix, new_side_size, {start.raw, start.col + new_side_size}, { new_side_size - 1, 0});
+    else
+    {
+        solve(matrix, new_side_size, {start.raw + new_side_size, start.col}, {point.raw - new_side_size, point.col});
+        create_figure(matrix, start, new_side_size, 4);
+        solve(matrix, new_side_size, start, {new_side_size - 1, new_side_size - 1});
+        solve(matrix, new_side_size, {start.raw + new_side_size, start.col + new_side_size}, {0, 0});
+        solve(matrix, new_side_size, {start.raw, start.col + new_side_size}, {new_side_size - 1, 0});
     }
 
 }
@@ -106,28 +124,26 @@ void solve(uint32_t ** matrix, uint32_t side_size, coord_t start, coord_t point)
 int main()
 {
 
-    uint32_t ** matrix, side_size;
-    uint8_t n;
+    int ** matrix, side_size;
+    int n;
     coord_t point;
-    scanf("%hhu %u %u", &n, &point.raw, &point.col);
-    side_size = (uint32_t) pow(2, n);
+    scanf("%d %d %d", &n, &point.raw, &point.col);
+    side_size = (int) pow(2, n);
 
-    matrix = new uint32_t * [side_size];
-    for(uint32_t i = 0; i < side_size; i++)
-    {
-        matrix[i] = new uint32_t[side_size];
-        for (uint32_t j = 0; j < side_size; j++)
-            matrix[i][j] = 0;
-    }
+    matrix = new int * [side_size];
+    for(int i = 0; i < side_size; i++)
+        matrix[i] = new int[side_size];
 
-    point.raw--; point.col--;
+    point.raw--;
+    point.col--;
+    matrix[point.raw][point.col] = 0;
+
     solve(matrix, side_size, {0, 0}, point);
 
-    for(uint32_t i = 0; i < side_size; i++)
+    for(int i = 0; i < side_size; i++)
     {
-        for(uint32_t j = 0; j < side_size; j++)
+        for(int j = 0; j < side_size; j++)
             printf("%2d ", matrix[i][j]);
-
         printf("\n");
     }
     return 0;
