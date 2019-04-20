cadba97d5155a8cf4ea5f604274f170ffcdfdacd
allacee <ddimaglushkov@ya.ru>
2019-04-21 01:25:59 +0300

    1067 added

--- MODULE_3/P3210_DGlushkov_1067.cpp
+++ MODULE_3/P3210_DGlushkov_1067.cpp
@@ -11,8 +11,9 @@ struct Node
     uint8_t level;
     map<string, Node> child;
 
-protected:
-    vector<string> parse_path_str(string& path_str)
+private:
+
+    static vector<string> parse_path_str(string& path_str)
     {
         vector<string> path;
         for (uint8_t last = 0, first; last < path_str.size(); last++)
@@ -26,11 +27,11 @@ protected:
     }
 
 
-
 public:
 
     Node(string str, uint32_t level, map<string, Node> child) : str(std::move(str)), level(level), child(std::move(child))
     {}
+
 
     void insert_path (string& path_str)
     {
@@ -39,23 +40,29 @@ public:
 
         for (uint8_t i = 0; i < paths.size(); i++)
         {
-            if (cur_node->child.count(paths[i])) // if already exists
+            if (cur_node->child.count(paths[i]))
                 cur_node = &(cur_node->child.at(paths[i]));
 
             else
             {
-                //cur_node->child[paths[i]] = Node(paths[i], i, map<string, Node>());
                 cur_node->child.insert(make_pair( paths[i], Node(paths[i], i,  map<string, Node>())));
                 cur_node = &(cur_node->child.at(paths[i]));
             }
         }
     }
 
+
     static ostream& do_print(ostream& os, const Node& node)
     {
-        if (!node.child.empty())
+        if (node.child.empty())
         {
-            for(uint8_t j = 0; j < node.level; j++)
+            for (uint8_t i = 0; i < node.level; i++)
+                os << ' ';
+            os << node.str << '\n';
+        }
+        else
+        {
+            for (uint8_t i = 0; i < node.level; i++)
                 os << ' ';
             os << node.str << '\n';
             for(const auto &cur_child : node.child)
@@ -65,33 +72,15 @@ public:
     }
 
 
-//        if (node.child.empty())
-//        {
-//            for (uint8_t i = 0; i < node.level; i++)
-//                os << ' ';
-//            os << node.str << '\n';
-//        }
-//        else
-//        {
-//            for (uint8_t i = 0; i < node.level; i++)
-//                os << ' ';
-//            os << node.str << '\n';
-//            for(const auto &cur_child : node.child)
-//                do_print(os, node.child.at(cur_child.first));
-//
-//
-//        }
-//        return os;
-
     friend ostream& operator<<(ostream& os, const Node& node);
-
 
 };
 
 ostream &operator<<(ostream &os, const Node& node)
 {
-//    return Node::do_print(os, node.child.begin().operator*().second);
-    return Node::do_print(os, node);
+    for(const auto &cur_child : node.child)
+        Node::do_print(os, node.child.at(cur_child.first));
+    return os;
 }
 
 
@@ -107,6 +96,5 @@ int main()
         root.insert_path(temp_path);
     }
     cout << root;
-
     return 0;
 }
