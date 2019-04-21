2f2886aec3bdd0919b475776460c836054e17d73
allacee <ddimaglushkov@ya.ru>
2019-04-21 16:28:38 +0300

    1494 updated

--- MODULE_3/P3210_DGlushkov_1494.cpp
+++ MODULE_3/P3210_DGlushkov_1494.cpp
@@ -1,76 +1,85 @@
 #include <iostream>
 #include <stack>
 #include <cstdint>
-#include <vector>
 
-using namespace std;
+template <class T, class Size>
+class Stack{
+
+    std::stack<T> stack;
+
+public:
+    Size max_size = 0;
+
+    void push(T value)
+    {
+        if (stack.size() < max_size)
+            stack.push(value);
+    }
+
+    Size size()
+    {
+        return stack.size();
+    }
+
+    T pop()
+    {
+        T temp = stack.top();
+        stack.pop();
+        return temp;
+    }
+
+    T top()
+    {
+        return stack.top();
+    }
+
+    bool empty()
+    {
+        return stack.empty();
+    }
+
+    bool full()
+    {
+        return stack.size() == max_size;
+    }
+
+    void fill(T begin, T end)
+    {
+        for (T i = begin; i < end; i++)
+            stack.push(i);
+    }
+
+};
 
 
 int main()
 {
-    uint32_t N, temp;
-    int counter_lower = 0, counter_bigger = 0;
-    stack<uint32_t> main;
-    vector<int> lower, bigger;
+    Stack<uint32_t, uint32_t> stack;
+    uint32_t prev, cur;
 
-    cin >> N;
+    std::cin >> stack.max_size;
 
-    lower = vector<int>(N / 2 + 1);
-    bigger = vector<int>(N / 2 + 1);
+    std::cin >> prev;
+    stack.fill(1, prev);
 
-    for (uint32_t i = 0; i < N; i++)
+    for (uint32_t i = 1; i < stack.max_size; i++)
     {
-        cin >> temp;
-        main.push(temp);
-    }
+        std::cin >> cur;
 
-//    temp = N % 2 == 0 ? N / 2 : N / 2 + 1;
-
-    if (!(N % 2))
-    {
-        temp = N / 2 + 1;
-        for (uint32_t i = 0; i < N; i++)
+        if (cur > prev)
         {
-            if(main.top() > temp)
-                bigger.push_back(main.top());
-            else if(main.top() < temp)
-                lower.push_back(main.top());
-            main.pop();
+            stack.fill(prev + 1, cur);
+            prev = cur;
+            continue;
         }
-    }
-    else
-    {
-        temp = N / 2;
-        for (uint32_t i = 0; i < N; i++)
+        else
+        if (stack.pop() != cur)
         {
-            if(main.top() <= temp)
-                lower.push_back(main.top());
-            else
-                bigger.push_back(main.top());
-            main.pop();
+            std::cout << "Cheater";
+            return 0;
         }
     }
 
-    for (uint32_t i = 1; i < N; i++)
-    {
-        if (lower[i] > lower [i - 1])
-            counter_lower++;
-        else
-            counter_lower--;
-
-        if (bigger[i] > bigger [i - 1])
-            counter_bigger++;
-        else
-            counter_bigger--;
-    }
-
-    for (auto it: lower)
-        cout << it << " ";
-
-    if (counter_bigger - counter_lower == N || counter_bigger - counter_lower == -N || counter_bigger - counter_lower == 0)
-        cout << "Not a proof";
-    else
-        cout << "Cheater";
-
+    std::cout << "Not a proof";
     return 0;
 }
