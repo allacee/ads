d0df8227eda8b4c9a77a66dca7763ec8570c80f6
allacee <ddimaglushkov@ya.ru>
2019-04-25 15:11:17 +0300

    1521 constructor done

--- MODULE_3/P3210_DGlushkov_1521.cpp
+++ MODULE_3/P3210_DGlushkov_1521.cpp
@@ -10,41 +10,71 @@ class Tree
     Size size;
 
 public:
-    Size cur_id;
+    //Size cur_id;
 
 private:
 
-    void to_left_son()
+    bool to_left_son(Size& cur_id)
     {
         if (2 * cur_id + 2 < size)
+        {
             cur_id = 2 * cur_id + 1;
+            return true;
+        }
+        else return false;
     }
 
-    void to_right_son()
+    bool to_right_son(Size& cur_id)
     {
         if (2 * cur_id + 2 < size)
+        {
             cur_id = 2 * cur_id + 2;
+            return true;
+        }
+        else return false;
     }
 
-    void to_father()
+    bool to_father(Size& cur_id)
     {
         if (cur_id != 0)
+        {
             cur_id = (cur_id - 1) / 2;
+            return true;
+        }
+        else return false;
     }
 
-    void to_brother()
+    bool to_brother(Size& cur_id)
     {
         if (cur_id != 0)
         {
             if(cur_id % 2)
             {
-                to_father();
-                to_right_son();
-            } else
-            {
-                to_father();
-                to_left_son();
+                to_father(cur_id);
+                to_right_son(cur_id);
             }
+            else
+            {
+                to_father(cur_id);
+                to_left_son(cur_id);
+            }
+            return true;
+        }
+        else return false;
+    }
+
+    void fill(Size cur_id, int temp_N, int temp_last_level)
+    {
+        Size temp_cur_id = cur_id;
+        values[temp_cur_id] = temp_N;
+
+        if (temp_cur_id < last_level >> 2u)
+        {
+            to_left_son(temp_cur_id);
+            fill(temp_cur_id, (temp_last_level / 2), (temp_last_level / 2));
+
+            to_brother(temp_cur_id);
+            fill(temp_cur_id, temp_N - (temp_last_level / 2), (temp_last_level / 2));
         }
     }
 
@@ -56,20 +86,30 @@ public:
         while (last_level < N)
             last_level = last_level << 1u;
 
-        /// check if this -1 is needed
-        size = last_level << 1u - 1;
+        size = (last_level << 1u) - 1;
         values = new T[size];
-        cur_id = 0;
 
-        //TODO: после заполнения дерева переставить указатель на самый левый элемент
+        // TODO: replace recursion with cycles
+        fill(0, N, last_level);
+
+        for (Size i = 0; i < N; i++)
+            values[i + last_level - 1] = 1;
+
+        for (Size i = N; i < last_level; i++)
+            values[i + last_level - 1] = 0;
+
 
     }
 
-    // переместиться из текущей ячейки в ячейку += k
-    // при этом мы перемешаемся только по нижнему уровню
-
-
-
+    void print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            std::cout << values[i] << " ";
+            if (i == 0 || i == 2 || i == 6 || i == 14 || i == 30 || i ==31)
+                std::cout << std::endl;
+        }
+    }
 
 
 
@@ -79,8 +119,11 @@ public:
 
 int main()
 {
-//    Tree<int, int> tree = Tree<int, int>(6);
+    Tree<int, int> tree = Tree<int, int>(3);
 
+//    tree.test();
+//    std::cout << tree.cur_id;
 
+    tree.print();
     return 0;
 }
