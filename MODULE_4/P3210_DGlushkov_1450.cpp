1a764a8cce37c37cc184f341ccf141dbde9ee9cf
allacee <ddimaglushkov@ya.ru>
2019-05-08 21:31:07 +0300

    negative edges added

--- MODULE_4/P3210_DGlushkov_1450.cpp
+++ MODULE_4/P3210_DGlushkov_1450.cpp
@@ -8,6 +8,9 @@ using namespace std;
 template <class T, class S>
 class Graph
 {
+
+//TODO: REMOVE
+public:
     map<S, map<S, T>> edges;
     S nof_nodes;
     vector<T> max_path;
@@ -17,7 +20,8 @@ class Graph
     void topological_sort(S cur_node = 0)
     {
         for (auto it : edges[cur_node])
-            topological_sort(it.first);
+            if (it.second > 0)
+                topological_sort(it.first);
 
         if(!checked[cur_node])
         {
@@ -41,6 +45,7 @@ public:
     void insert_edge(S from, S to, T weight)
     {
         edges[from].insert({to, weight});
+        edges[to].insert({from, -1*weight});
     }
 
     void solve(S root)
@@ -54,6 +59,16 @@ public:
             prev_node = cur_node;
             cur_node = top_order.top();
             top_order.pop();
+
+            for (auto it : edges[cur_node])
+            {
+                if (it.second < 0)
+                {
+                    S temp = max_path[it.first] - it.second;
+                    if (temp > max_path[cur_node])
+                        max_path[cur_node] = temp;
+                }
+            }
 
         }
     }
@@ -74,11 +89,11 @@ public:
 
 int main()
 {
-    uint32_t nof_nodes, nof_edges, node_from, node_to, node_weight;
+    int nof_nodes, nof_edges, node_from, node_to, node_weight;
     cin >> nof_nodes >> nof_edges;
-    Graph<uint32_t, uint32_t> graph(nof_nodes);
+    Graph<int, int> graph(nof_nodes);
 
-    for (uint32_t i = 0; i < nof_edges; i++)
+    for (int i = 0; i < nof_edges; i++)
     {
         cin >> node_from >> node_to >> node_weight;
         graph.insert_edge(node_from - 1, node_to - 1, node_weight);
@@ -87,6 +102,7 @@ int main()
     cin >> node_from >> node_to;
 
     graph.solve(node_from - 1);
+//    graph.topological_sort(node_from - 1);
     graph.print_result(node_to - 1);
     return 0;
 }
