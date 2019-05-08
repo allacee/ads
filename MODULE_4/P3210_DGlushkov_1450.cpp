7cc29dd637f54193b5070cd241c30e99ad035d00
allacee <ddimaglushkov@ya.ru>
2019-05-09 02:47:07 +0300

    1450: I'm about to commit suicide

--- MODULE_4/P3210_DGlushkov_1450.cpp
+++ MODULE_4/P3210_DGlushkov_1450.cpp
@@ -1,86 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <stack>
 
 using namespace std;
 
-template <class T, class S>
+template<class T, class S>
 class Graph
 {
-
-//TODO: REMOVE
-public:
-    map<S, map<S, T>> edges;
-    S nof_nodes;
+    struct edge{
+        S from, to;
+        T weight;
+    };
+    vector<edge> edges;
     vector<T> max_path;
-    stack<S> top_order;
-    bool * checked;
-
-    void topological_sort(S cur_node = 0)
-    {
-        for (auto it : edges[cur_node])
-            if (it.second > 0)
-                topological_sort(it.first);
-
-        if(!checked[cur_node])
-        {
-            checked[cur_node] = true;
-            top_order.push(cur_node);
-        }
-    }
+    S nof_nodes, nof_edges, nof_checked;
 
 public:
-    explicit Graph(S nof_nodes): nof_nodes(nof_nodes)
+
+    explicit Graph(S nof_nodes, S nof_edges): nof_nodes(nof_nodes), nof_edges(nof_edges)
     {
-        max_path = vector<T>(nof_nodes, 0);
-        checked = new bool[nof_nodes];
-        for (S i = 0; i < nof_nodes; i++)
-        {
-            edges[i] = map<S, T>();
-            checked[i] = false;
-        }
+        nof_checked = 0;
+//        edges = vector<edge>(nof_edges);
+        max_path = vector<T>(nof_nodes, -1);
     }
 
-    void insert_edge(S from, S to, T weight)
+    void insert(S from, S to, T weight)
     {
-        edges[from].insert({to, weight});
-        edges[to].insert({from, -1*weight});
+        edges.push_back({from, to, weight});
     }
 
-    void solve(S root)
+    T solve(S start_node, S end_node)
     {
-        topological_sort(root);
+        max_path[start_node] = 0;
 
-        S prev_node, cur_node;
-
-        while (!top_order.empty())
-        {
-            prev_node = cur_node;
-            cur_node = top_order.top();
-            top_order.pop();
-
-            for (auto it : edges[cur_node])
-            {
-                if (it.second < 0)
+        for (S i = 0; i < nof_checked; i++)
+            for (S j = 0; j < nof_edges; j++)
+                if (max_path[edges[j].from] != -1 && max_path[edges[j].to] < max_path[edges[j].from] + edges[j].weight)
                 {
-                    S temp = max_path[it.first] - it.second;
-                    if (temp > max_path[cur_node])
-                        max_path[cur_node] = temp;
+                    max_path[edges[j].to] = max_path[edges[j].from] + edges[j].weight;
+                    nof_checked ++;
                 }
-            }
-
-        }
-    }
-
-
-
-    void print_result(S destination)
-    {
-        if (max_path[destination])
-            cout << max_path[destination];
-        else
-            cout << "No solution";
+        return max_path[end_node];
     }
 
 
@@ -89,20 +48,26 @@ public:
 
 int main()
 {
-    int nof_nodes, nof_edges, node_from, node_to, node_weight;
-    cin >> nof_nodes >> nof_edges;
-    Graph<int, int> graph(nof_nodes);
+    uint32_t nof_nodes, nof_edges, node_from, node_to, start, end;
+    int node_weight, result;
 
-    for (int i = 0; i < nof_edges; i++)
+    cin >> nof_nodes >> nof_edges;
+
+    Graph<int, uint32_t> graph = Graph<int, uint32_t>(nof_nodes, nof_edges);
+
+    for (uint32_t i = 0; i < nof_edges; i++)
     {
         cin >> node_from >> node_to >> node_weight;
-        graph.insert_edge(node_from - 1, node_to - 1, node_weight);
+        graph.insert(node_from - 1, node_to - 1, node_weight);
     }
 
-    cin >> node_from >> node_to;
+    cin >> start >> end;
 
-    graph.solve(node_from - 1);
-//    graph.topological_sort(node_from - 1);
-    graph.print_result(node_to - 1);
+
+    if ((result = graph.solve(start - 1, end - 1)) == -1)
+        cout << "No solution";
+    else
+        cout << result;
+
     return 0;
 }
