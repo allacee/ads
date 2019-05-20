b458ef2792536eb91ba14796885b3b2a9ab560b3
allacee <ddimaglushkov@ya.ru>
2019-05-20 13:19:06 +0300

    1806 WA8

--- MODULE_4/P3210_DGlushkov_1806.cpp
+++ MODULE_4/P3210_DGlushkov_1806.cpp
@@ -11,27 +11,27 @@ using namespace std;
 
 class Graph
 {
-    vector<int16_t> price;
-    unordered_map<string, pair<int16_t, unordered_map<string, int16_t>>> edges;
+    vector<int> price;
+    unordered_map<string, pair<int, unordered_map<string, int>>> edges;
     vector<string> id_to_num;
-    vector<int32_t> length;
-    stack<int16_t> path;
-    vector<int16_t> parents;
+    vector<int> length;
+    stack<int> path;
+    vector<int> parents;
 
-    int32_t n, iter;
+    int n, iter;
 
 
 public:
 
-    explicit Graph(int32_t n): n(n)
+    explicit Graph(int n): n(n)
     {
-        length = vector<int32_t>(n, INF);
-        parents = vector<int16_t>(n);
-        id_to_num = vector<string>(n);
+        length = vector<int>(2*n, INF);
+        parents = vector<int>(2*n);
+        id_to_num = vector<string>(2*n);
         iter = 0;
     }
 
-    void insert_price(int16_t val)
+    void insert_price(int val)
     {
         price.push_back(val);
     }
@@ -43,50 +43,46 @@ public:
         edges[val].first = edges.size();
         id_to_num[iter++] = val;
 
-        for(auto & it: edges)
-            //it.first <- следующий узел
-            for (int16_t i = 0, first_diff = 0, counter = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++)
+        {
+            for(int j = i + 1; j < 10; j++)
             {
-                if (val[i] != it.first[i])
+                string temp = val;
+                swap(temp[i], temp[j]);
+                if(edges.find(temp) != edges.end() && temp != val)
                 {
-                    if (counter == 0)
+                    edges[val].second.insert({temp, price[i]});
+                    edges[temp].second.insert({val, price[i]});
+                }
+            }
+
+            for(char e = 48; e < 58; e++)
+            {
+                if(val[i] != e)
+                {
+                    string temp = val;
+                    temp[i] = e;
+                    if(edges.find(temp) != edges.end())
                     {
-                        string temp = val;
-                        temp[i] = it.first[i];
-                        first_diff = i;
-                        if (temp == it.first)
-                        {
-                            it.second.second.insert({val, price[first_diff]});
-                            edges[val].second.insert({it.first, price[first_diff]});
-                            break;
-                        }
-                        else
-                            counter++;
-                    }
-                    else if (counter == 1)
-                    {
-                        string temp = val;
-                        swap(temp[first_diff], temp[i]);
-                        if (temp == it.first)
-                        {
-                            it.second.second.insert({val, price[first_diff]});
-                            edges[val].second.insert({it.first, price[first_diff]});
-                        }
-                        break;
+                        edges[val].second.insert({temp, price[i]});
+                        edges[temp].second.insert({val, price[i]});
                     }
                 }
             }
+
+        }
+
     }
 
     void solve()
     {
-        vector<bool> checked = vector<bool>(n, false);
+        vector<bool> checked = vector<bool>(2*n, false);
 
         length[0] = 0;
-        for (int32_t i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            int32_t v = -1;
-            for (int32_t j = 0; j < n; j++)
+            int v = -1;
+            for (int j = 0; j < n; j++)
                 if(!checked[j] && (v == -1 || length[j] < length[v]))
                     v = j;
 
@@ -94,9 +90,9 @@ public:
                 break;
             checked[v] = true;
 
-            for (const auto& it : edges[id_to_num[v]].second)
+            for (auto it : edges[id_to_num[v]].second)
             {
-                int16_t to = edges[it.first].first, len = it.second;
+                int to = edges[it.first].first, len = it.second;
                 if (length[v] + len < length[to])
                 {
                     length[to] = length[v] + len;
@@ -108,7 +104,7 @@ public:
         }
     }
 
-    void print_result(int16_t end)
+    void print_result(int end)
     {
 
         if (length[end] == INF)
@@ -117,12 +113,11 @@ public:
             return;
         }
 
-        for (int16_t i = end; i != 0; i = parents[i])
+        for (int i = end; i != 0; i = parents[i])
             path.push(i);
         path.push(0);
 
-        cout << length[end] << '\n'
-        << path.size() << '\n';
+        cout << length[end] << '\n' << path.size() << '\n';
         while(!path.empty())
         {
             cout << path.top() + 1 << " ";
@@ -139,20 +134,20 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int32_t n;
-    int16_t price;
+    int n;
+    int price;
     string num;
 
     cin >> n;
     Graph graph = Graph(n);
 
-    for (int16_t i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
     {
         cin >> price;
         graph.insert_price(price);
     }
 
-    for (int16_t i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> num;
         graph.insert_num(num);
