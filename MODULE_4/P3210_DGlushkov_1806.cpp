2215aacc3611f23c1e2e35e404c2cad84d9ae816
allacee <ddimaglushkov@ya.ru>
2019-05-20 07:17:47 +0300

    1806 1 part done

--- MODULE_4/P3210_DGlushkov_1806.cpp
+++ MODULE_4/P3210_DGlushkov_1806.cpp
@@ -6,49 +6,35 @@ using namespace std;
 
 class Graph
 {
-    vector<int16_t> price = vector<int16_t>(10);
+    vector<int16_t> price;
     map<int64_t, map<int64_t, int16_t>> edges;
 
-    pair<int16_t, int16_t> compare_num(string & str1, string & str2)
+    static pair<int16_t, int16_t> compare_nums(int64_t num1, int64_t num2, bool & conn)
     {
         int16_t first_diff = -1, num_of_diff = 0;
-        for (int16_t i = 0; i < str1.size(); i++)
-        {
-            if(str1[i] != str2[i])
-            {
-                num_of_diff++;
-                if(first_diff == -1)
-                    first_diff = i;
-            }
-        }
-        return {first_diff, num_of_diff};
-    }
-
-    pair<int16_t, int16_t> compare_nums(int64_t num1, int16_t num2)
-    {
-        int16_t first_diff = -1, num_of_diff = 0;
+        pair<int16_t, int16_t> first = {-1, -1}, second = {-1, -1};
         for(int16_t i = 9; num1 > 0 || num2 > 0; num1/=10, num2/=10, i--)
         {
             if (num1 % 10 != num2 % 10)
             {
                 num_of_diff++;
                 first_diff = i;
+                first.first = num1 % 10;
+                second.first = num2 % 10;
+                if (first.second == -1)
+                {
+                    first.second = num1 % 10;
+                    second.second = num2 % 10;
+                }
             }
         }
 
+        if (num_of_diff == 1 || (num_of_diff == 2 && first.first == second.second && first.second == second.first))
+            conn = true;
         return {first_diff, num_of_diff};
     }
 
-    int64_t str_to_int64(string & str)
-    {
-        int64_t num = 0;
-        for(char ch : str)
-            num = num * 10 + ch - '0';
-        return num;
-    }
-
 public:
-
 
     void insert_price(int16_t val)
     {
@@ -57,20 +43,21 @@ public:
 
     void insert_num(int64_t val)
     {
+        edges[val];
+        bool conn = false;
 
-        for(auto it: edges)
+        for(auto & it: edges)
         {
-            auto diff = compare_nums(val, it.first);
-            if (diff.second == 1)
+            auto diff = compare_nums(val, it.first, conn);
+            if (conn)
             {
                 it.second.insert({val, price[diff.first]});
                 edges[val].insert({it.first, price[diff.first]});
+                conn = false;
             }
         }
 
     }
-
-
 
 };
 
@@ -80,12 +67,24 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int16_t n;
-    string num;
+    uint16_t n;
+    int16_t price;
+    int64_t num;
 
     cin >> n;
     Graph graph = Graph();
 
+    for (int16_t i = 0; i < 10; i++)
+    {
+        cin >> price;
+        graph.insert_price(price);
+    }
+
+    for (uint16_t i = 0; i < n; i++)
+    {
+        cin >> num;
+        graph.insert_num(num);
+    }
 
 
 
