42e606b8c269401ce23d41cef55d9ed274becd37
allacee <ddimaglushkov@ya.ru>
2019-05-22 05:56:09 +0300

    1806 string -> int64

--- MODULE_4/P3210_DGlushkov_1806.cpp
+++ MODULE_4/P3210_DGlushkov_1806.cpp
@@ -1,73 +1,92 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <stack>
 #include <unordered_map>
 #include <algorithm>
+#include <deque>
+#include <queue>
+#include <cmath>
 
-#define INF 10001
+#define INF 64000u
 
 using namespace std;
 
 class Graph
 {
-    vector<int> price;
-    unordered_map<string, pair<int, unordered_map<string, int>>> edges;
-    vector<string> id_to_num;
-    vector<int> length;
-    stack<int> path;
-    vector<int> parents;
+    vector<uint16_t> price;
+    unordered_map<int64_t, pair<uint16_t, unordered_map<int64_t, uint16_t>>> edges;
+    vector<int64_t> id_to_num;
+    vector<uint16_t> length;
+    deque<uint16_t> path;
+    vector<uint16_t> parents;
+    uint16_t n, iter;
 
-    int n, iter;
+    inline int64_t swap_digits(int64_t num, int16_t src, int16_t dst)
+    {
+        int64_t res = num;
+        auto pow1 = (int64_t)(pow(10, 9 - src)), pos2 = (int64_t)(pow(10, 9 - dst));
+        int16_t dig1 = (num / pow1) % 10, dig2 = (num / pos2) % 10;
+        res -= pow1 * dig1;
+        res -= pos2 * dig2;
+        res += pos2 * dig1;
+        res += pow1 * dig2;
+        return res;
+    }
+
+    inline int64_t change_digit(int64_t num, int16_t pos, int16_t new_val)
+    {
+        int64_t res = num;
+        auto pow1 = (int64_t)(pow(10, 9 - pos));
+        int16_t dig1 = (num / pow1) % 10;
+        res -= (dig1 - new_val) * pow1;
+        return res;
+    }
 
 
 public:
 
-    explicit Graph(int n): n(n)
+    explicit Graph(uint16_t n): n(n)
     {
-        length = vector<int>(2*n, INF);
-        parents = vector<int>(2*n);
-        id_to_num = vector<string>(2*n);
+        length = vector<uint16_t>(n + 5, INF);
+        parents = vector<uint16_t>(n + 5);
+        id_to_num = vector<int64_t>(n + 5);
         iter = 0;
     }
 
-    void insert_price(int val)
+    void insert_price(uint16_t val)
     {
         price.push_back(val);
     }
 
-    void insert_num(string & val)
+    void insert_num(int64_t & val_num)
     {
-        bool conn = false;
 
-        edges[val].first = edges.size();
-        id_to_num[iter++] = val;
+        edges[val_num].first = edges.size();
+        id_to_num[iter++] = val_num;
 
-        for (int i = 0; i < 10; i++)
+        for (int16_t i = 0; i < 10; i++)
         {
-            for(int j = i + 1; j < 10; j++)
+            for(int16_t j = i + 1; j < 10; j++)
             {
-                string temp = val;
-                swap(temp[i], temp[j]);
-                if(edges.find(temp) != edges.end() && temp != val)
+                int64_t temp = swap_digits(val_num, i, j);
+
+                if(edges.find(temp) != edges.end() && temp != val_num)
                 {
-                    edges[val].second.insert({temp, price[i]});
-                    edges[temp].second.insert({val, price[i]});
+                    edges[val_num].second.insert({temp, price[i]});
+                    edges[temp].second.insert({val_num, price[i]});
                 }
             }
 
-            for(char e = 48; e < 58; e++)
+            for(int16_t ch = 0; ch < 9; ch++)
             {
-                if(val[i] != e)
+
+                int64_t temp = change_digit(val_num, i, ch);
+                if(edges.find(temp) != edges.end() && temp != val_num)
                 {
-                    string temp = val;
-                    temp[i] = e;
-                    if(edges.find(temp) != edges.end())
-                    {
-                        edges[val].second.insert({temp, price[i]});
-                        edges[temp].second.insert({val, price[i]});
-                    }
+                    edges[val_num].second.insert({temp, price[i]});
+                    edges[temp].second.insert({val_num, price[i]});
                 }
+
             }
 
         }
@@ -76,35 +95,31 @@ public:
 
     void solve()
     {
-        vector<bool> checked = vector<bool>(2*n, false);
+        priority_queue <pair<uint16_t, uint16_t>> que;
 
         length[0] = 0;
-        for (int i = 0; i < n; i++)
+        que.push({0, 0});
+        while(!que.empty())
         {
-            int v = -1;
-            for (int j = 0; j < n; j++)
-                if(!checked[j] && (v == -1 || length[j] < length[v]))
-                    v = j;
-
-            if(length[v] == INF)
-                break;
-            checked[v] = true;
+            uint16_t v = que.top().second;
+            que.pop();
 
             for (auto it : edges[id_to_num[v]].second)
             {
-                int to = edges[it.first].first, len = it.second;
+                uint16_t to = edges[it.first].first, len = it.second;
                 if (length[v] + len < length[to])
                 {
                     length[to] = length[v] + len;
                     parents[to] = v;
+                    que.push({length[to], to});
                 }
             }
-            if (v == n - 1)
-                break;
         }
     }
 
-    void print_result(int end)
+
+
+    void print_result(uint16_t end)
     {
 
         if (length[end] == INF)
@@ -113,17 +128,13 @@ public:
             return;
         }
 
-        for (int i = end; i != 0; i = parents[i])
-            path.push(i);
-        path.push(0);
+        for (int64_t i = end; i != 0; i = parents[i])
+            path.push_front(i);
+        path.push_front(0);
 
         cout << length[end] << '\n' << path.size() << '\n';
-        while(!path.empty())
-        {
-            cout << path.top() + 1 << " ";
-            path.pop();
-        }
-
+        for (auto i : path)
+            cout << i + 1 << " ";
     }
 
 };
@@ -134,20 +145,20 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int n;
-    int price;
-    string num;
+    uint16_t n;
+    uint16_t price;
+    int64_t num;
 
     cin >> n;
     Graph graph = Graph(n);
 
-    for (int i = 0; i < 10; i++)
+    for (int16_t i = 0; i < 10; i++)
     {
         cin >> price;
         graph.insert_price(price);
     }
 
-    for (int i = 0; i < n; i++)
+    for (uint16_t i = 0; i < n; i++)
     {
         cin >> num;
         graph.insert_num(num);
